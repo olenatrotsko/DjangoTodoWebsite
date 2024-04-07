d13e9a8c471b4b5ad675d2f3eb6f0419d42d7fe0
trolchiha <trotsko.olena@gmail.com>
2024-04-07 16:00:19 +0300

    Added test_models fot authentication and todo

--- todo/tests/test_models.py
+++ todo/tests/test_models.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from authentication.models import User
+from ..models import Todo
 
 class TestModels(TestCase):
-    def test_should_create_user(self):
+    def test_should_create_todo(self):
         user = User.objects.create_user(
             username='testuser',
             email='testuser@email.com',
@@ -10,6 +11,13 @@ class TestModels(TestCase):
         user.set_password('password123')
         user.save()
 
-        self.assertEqual(str(user), 'testuser@email.com')
+        todo = Todo(
+            title='Test Todo',
+            description='Test Description',
+            owner=user
+        )
+        todo.save()
+
+        self.assertEqual(str(todo), 'Test Todo')
 
             
